756fe2a7c7513e6f57981fac4bdeb1741982e129
Lotosney <sebastiandwojcik@gmail.com>
2021-05-12 14:03:53 +0200

    Add operation handling f.ex substract

--- script.js
+++ script.js
@@ -1,40 +1,90 @@
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
-      this.previousOperandTextElement = previousOperandTextElement
-      this.currentOperandTextElement = currentOperandTextElement
-      this.clear()
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
-  
+
     clear() {
-      this.currentOperand = ''
-      this.previousOperand = ''
-      this.operation = undefined
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
-  
+
     delete() {
+ 
     }
-  
+
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand =this.currentOperand.toString() + number.toString()
-     
-    }
-  
-    chooseOperation(operation) {
-  
-    }
-  
-    compute() {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
 
     }
-  
-  
-  
-    updateDisplay() {
-      this.currentOperandTextElement.innerText =this.currentOperand
-      
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+          this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+          case '+':
+            computation = prev + current
+            break
+          case '-':
+            computation = prev - current
+            break
+          case '*':
+            computation = prev * current
+            break
+          case '÷':
+            computation = prev / current
+            break
+          default:
+            return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+
+    }
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
     }
   }
+
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+        if(this.operation!= null){
+            this.previousOperandTextElement.innerText = 
+            `${this.previousOperand} ${this.operation}`
+        }
+        
+
+    }
+}
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -48,7 +98,18 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-      calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
   })
