95637fdc856ddfea79058e75298cb000057ff305
Lotosney <sebastiandwojcik@gmail.com>
2021-05-12 14:11:58 +0200

    Add delete and clear all handling

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
- 
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -111,5 +111,15 @@ operationButtons.forEach(button => {
 })
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+  })
+
+  deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
+
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
     calculator.updateDisplay()
   })
